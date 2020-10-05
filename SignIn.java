48e868e5f86ba7673cb2544b7dd22086bddce521
et <et@et.com>
2020-10-05 22:01:56 +0800

    在公司开发

--- SignIn.java
+++ SignIn.java
@@ -1,8 +1,8 @@
 public class SignIn{
-	private static String id;
 	private static String pwd;
 	private static String name;
 	public static void main(String[] args){
 		System.out.println("name");
+		System.out.println("pwd");
 	}
 }
